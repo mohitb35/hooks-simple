89eb886a0eb48ad534528ba10dd81c46e78d2d92
Mohit Bajaj <mohit_b35@hotmail.com>
2020-06-17 13:29:24 +0530

    refactor class based App comp. to functional comp. - use the useState hook a. import useState b. call useState with initial value as param c. values returned by useState -> stateful value and setter d. array destructuring to store values returned by useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
+/* class App extends React.Component {
 	state = {
 		resource: 'posts'
 	};
@@ -16,6 +16,19 @@ class App extends React.Component {
 			</div>
 		);
 	}
+} */
+
+const App = () => {
+	const [resource, setResource] = useState('posts');
+	return(
+		<div className="ui container">
+			<div>
+				<button onClick={() => setResource('posts')}>Posts</button>
+				<button onClick={() => setResource('todos')}>Todos</button>
+			</div>
+			{resource}
+		</div>
+	);
 }
 
 export default App;
