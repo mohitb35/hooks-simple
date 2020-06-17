29f0ec05990dd8f956fe67f0dbc597d685a80b4d
Mohit Bajaj <mohit_b35@hotmail.com>
2020-06-17 12:52:02 +0530

    initially setup App as a class based component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+	state = {
+		resource: 'posts'
+	};
+
 	render() {
 		return(
-			<div>App</div>
+			<div class="ui container">
+				<div>
+					<button onClick={() => this.setState({ resource: 'posts' })}>Posts</button>
+					<button onClick={() => this.setState({ resource: 'todos' })}>Todos</button>
+				</div>
+				{this.state.resource}
+			</div>
 		);
 	}
 }
